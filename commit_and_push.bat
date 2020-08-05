39721fa8b1a705cbddba0e406669a3c1f7c7a42d
hoangthaiduong <hoangthaiduong@gmail.com>
2020-08-05 13:51:35 -0600

    add windows terminal settings

--- commit_and_push.bat
+++ commit_and_push.bat
@@ -1,5 +1,6 @@
 git config --global core.autocrlf false
-cp C:/cygwin64/home/User/.emacs .
+cp C:/cygwin64/home/User/.emacs ./emacs
+cp C:/Users/User/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json ./terminal
 git add -u .
 set /p msg="Enter commit message: "
 git commit -m "%msg%"
