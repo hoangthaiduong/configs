7a28dd7bc4dabdea1afa311549707290b886cd4d
hoangthaiduong <hoangthaiduong@gmail.com>
2020-08-05 14:20:47 -0600

    add doublecmd

--- commit_and_push.bat
+++ commit_and_push.bat
@@ -1,6 +1,7 @@
 git config --global core.autocrlf false
 cp C:/cygwin64/home/User/.emacs ./emacs
 cp C:/Users/User/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json ./terminal
+cp C:/Users/User/AppData/Roaming/doublecmd/doublecmd.xml ./doublecmd
 zip -r ./emacs/.emacs.d.zip C:/cygwin64/home/User/.emacs.d
 git add -A .
 set /p msg="Enter commit message: "
