ea6169703b9e858785ee4475defa901b7f1fa0d7
hoangthaiduong <hoangthaiduong@gmail.com>
2020-08-05 14:23:06 -0600

    add vscode

--- commit_and_push.bat
+++ commit_and_push.bat
@@ -2,6 +2,7 @@ git config --global core.autocrlf false
 cp C:/cygwin64/home/User/.emacs ./emacs
 cp C:/Users/User/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json ./terminal
 cp C:/Users/User/AppData/Roaming/doublecmd/doublecmd.xml ./doublecmd
+cp C:/Users/User/AppData/Roaming/Code/User/settings.json ./vscode
 zip -r ./emacs/.emacs.d.zip C:/cygwin64/home/User/.emacs.d
 git add -A .
 set /p msg="Enter commit message: "
