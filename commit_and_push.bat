4937eb14e5fe01b79cf01d89fbfb6c6d5ea2fabb
hoangthaiduong <hoangthaiduong@gmail.com>
2020-08-05 18:49:00 -0600

    use %userprofile% instead of explicit user path

--- commit_and_push.bat
+++ commit_and_push.bat
@@ -1,9 +1,9 @@
 git config --global core.autocrlf false
 cp C:/cygwin64/home/User/.emacs ./emacs
-cp C:/Users/User/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json ./terminal
-cp C:/Users/User/AppData/Roaming/doublecmd/doublecmd.xml ./doublecmd
-cp C:/Users/User/AppData/Roaming/Code/User/settings.json ./vscode
-cp C:/Users/User/scoop/apps/keypirinha/current/portable/Profile/User/Keypirinha.ini ./keypirinha
+cp %USERPROFILE%/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json ./terminal
+cp %USERPROFILE%/AppData/Roaming/doublecmd/doublecmd.xml ./doublecmd
+cp %USERPROFILE%/AppData/Roaming/Code/User/settings.json ./vscode
+cp %USERPROFILE%/scoop/apps/keypirinha/current/portable/Profile/User/Keypirinha.ini ./keypirinha
 zip -r ./emacs/.emacs.d.zip C:/cygwin64/home/User/.emacs.d
 git add -A .
 set /p msg="Enter commit message: "
